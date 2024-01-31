76f2197b50a510e1243109cbeafde3b8c8b82a84
Yamashita <c3260@st.cs.kumamoto-u.ac.jp>
2024-02-01 00:25:53 +0900

    make setup.sh

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # ros2_docker
 docker file of ros2
 
-setup.bash is install docker latest and pull docker images to use this.
+install_docker.bash is install docker latest.
---
+++ src/setup.sh
@@ -0,0 +1,34 @@
+#!/bin/bash -e
+
+#------------- add GPGkey
+apt-get install curl gnupg lsb-release -y
+curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -o /usr/share/keyrings/ros-archive-keyring.gpg
+
+#------------- add source list in repository
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(source /etc/os-release && echo $UBUNTU_CODENAME) main" | tee /etc/apt/sources.list.d/ros2.list > /dev/null
+
+#------------- install ros2
+sudo apt update
+sudo apt install ros-humble-desktop
+
+#------------- setup ros2
+echo "source /opt/ros/humble/setup.bash" >> ~/.bashrc
+source ~/.bashrc
+
+#------------- install rosdep and init
+sudo apt install python3-colcon-common-extensions
+sudo apt install python3-rosdep
+sudo rosdep init
+rosdep update
+
+#------------- make ws
+mkdir -p ~/ros2_ws/src
+cd ros2_ws/
+colcon build
+
+#------------- install gazebo and rqt
+sudo apt -y install gazebo
+sudo apt install ros-humble-gazebo-*
+sudo apt install ros-humble-rqt-*
+
+source ~/.bashrc
