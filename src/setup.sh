b259bd0ac751770e7d68d2ab2f05577d30c84b23
AoiYamashita <154947689+AoiYamashita@users.noreply.github.com>
2024-02-27 10:49:24 +0900

    update gazebo version

--- src/setup.sh
+++ src/setup.sh
@@ -27,8 +27,15 @@ cd ros2_ws/
 colcon build
 
 #------------- install gazebo and rqt
-apt -y install gazebo
-apt install ros-humble-gazebo-* -y
+sudo apt-get update
+sudo apt-get install lsb-release wget gnupg
+sudo wget https://packages.osrfoundation.org/gazebo.gpg -O /usr/share/keyrings/pkgs-osrf-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/pkgs-osrf-archive-keyring.gpg] http://packages.osrfoundation.org/gazebo/ubuntu-stable $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/gazebo-stable.list > /dev/null
+sudo apt-get update
+sudo apt-get install ignition-fortress
+
+#apt -y install gazebo
+#apt install ros-humble-gazebo-* -y
 apt install ros-humble-rqt-* -y
 
 source ~/.bashrc
